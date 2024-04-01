3e61bebf553a8219c6ea35b2b5a6a2569dec6912
leocastroz <castroz.dev@gmail.com>
2024-04-01 00:08:52 -0300

    refact: adjusting components and styles

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,15 @@
-<script setup lang="ts">
-import HelloWorld from './components/HelloWorld.vue'
-import { BeakerIcon, BoltIcon } from './icons'
+<script setup>
+import Header from './components/Header.vue';
 </script>
 
 <template>
-  <div>
+  <Header />
+  <div class="tw-text-white">
     <nav>
-      <RouterLink to="/">Go to Home</RouterLink>
+      <RouterLink to="/" class="tw-pr-5">Go to Home</RouterLink>
       <RouterLink to="/about">Go to About</RouterLink>
     </nav>
-    <p class="tw-bg-gradient-to-r tw-from-yellow-300 tw-to-orange-600 tw-p-3 tw-rounded-xl tw-text-white">Hello</p>
-    <BeakerIcon class="tw-h-6 tw-w-6 tw-text-orange-600" />
-    <BoltIcon class="tw-h-6 tw-w-6 tw-text-orange-600" />
   </div>
-  <HelloWorld msg="Vue-Hot-Toast" />
   <div>
     <RouterView />
   </div>
--- src/icons.ts
+++ src/icons.ts
@@ -1,5 +1,7 @@
 // add icons - https://heroicons.com/
 import { BeakerIcon } from '@heroicons/vue/24/solid'
 import { BoltIcon } from '@heroicons/vue/24/solid'
+import { MagnifyingGlassIcon } from '@heroicons/vue/24/solid'
+import { FireIcon } from '@heroicons/vue/24/solid'
 
-export { BeakerIcon, BoltIcon };
+export { BeakerIcon, BoltIcon, MagnifyingGlassIcon, FireIcon };
